a48f25b3f7a77ac5fb4f1bf47cad2bea3233a9f5
matryoshkaa <66214321+matryoshkaa@users.noreply.github.com>
2020-08-16 17:00:17 +0400

    Dashboard stuff

---
+++ app/src/main/java/com/example/curiosity/ScanPet.java
@@ -0,0 +1,14 @@
+package com.example.curiosity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScanPet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_pet);
+    }
+}
