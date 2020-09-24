b28ac244c6d87ff20ed78c292fea245f2910ed67
masafibro <arwaabdulhaq1d.aa@gmail.com>
2020-09-24 14:12:03 +0400

    fixed some nfc bugs

--- app/src/main/java/com/example/curiosity/ScanPet.java
+++ app/src/main/java/com/example/curiosity/ScanPet.java
@@ -17,29 +17,21 @@ public class ScanPet extends AppCompatActivity {
 
     ImageButton back_button;
     ImageButton settings_button;
+
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-//        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
-//            System.out.print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-//            Parcelable[] rawMessages =
-//                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-//
-//            if (rawMessages != null) {
-//                NdefMessage[] messages = new NdefMessage[rawMessages.length];
-//                for (int i = 0; i < rawMessages.length; i++) {
-//                    messages[i] = (NdefMessage) rawMessages[i];
-//                }
-//                // Process the messages array.
-//
-////                TextView textView = (TextView) findViewById(R.id.ScannedID);
-////                textView.setText(messages.toString());
-//
-//            }
-//        }
-
-        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-        System.out.println(tag);
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMessages =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMessages != null) {
+                NdefMessage[] messages = new NdefMessage[rawMessages.length];
+                for (int i = 0; i < rawMessages.length; i++) {
+                    messages[i] = (NdefMessage) rawMessages[i];
+                }
+                // Process the messages array.
+            }
+        }
     }
 
 
