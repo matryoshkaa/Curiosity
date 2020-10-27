042766ef2c0bd0c30e105afe2659c27651aa9ae3
feeyus <38934914+feeyus@users.noreply.github.com>
2020-10-27 14:35:26 +0400

    Added Select Pet to Settings

--- app/src/main/java/com/example/curiosity/Dashboard.java
+++ app/src/main/java/com/example/curiosity/Dashboard.java
@@ -1,12 +1,11 @@
 package com.example.curiosity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -17,6 +16,7 @@ public class Dashboard extends AppCompatActivity {
     ImageButton track_button;
     ImageButton pet_button;
 
+    String currentPet; //current selected pet from settings
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
