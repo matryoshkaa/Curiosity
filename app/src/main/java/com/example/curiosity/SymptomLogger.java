42539b4d1334dd2526cfbafc4ce269c9d5ec39de
masafibro <arwaabdulhaq1d.aa@gmail.com>
2020-11-21 00:32:39 +0400

    symptom logger stuff

--- app/src/main/java/com/example/curiosity/SymptomLogger.java
+++ app/src/main/java/com/example/curiosity/SymptomLogger.java
@@ -5,15 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SymptomLogger extends AppCompatActivity {
 
     ImageButton back_button;
     ImageButton settings_button;
-    //CheckBox cb = new CheckBox(this);
+    ListView symptomListView;
+
+    List symptoms = new ArrayList();
+    ArrayAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +35,19 @@ public class SymptomLogger extends AppCompatActivity {
 
         back_button=(ImageButton)findViewById(R.id.back_button);
         settings_button=(ImageButton)findViewById(R.id.settings_button);
+        symptomListView = (ListView) findViewById(R.id.symptomListView);
 
-//        cb = (CheckBox)findViewById((R.id.checkBox));
-//        cb.setText("Tutlane");
-//        cb.setChecked(true);
-//        //cb.addView(cb);
 
+
+        try{
+            addSymptoms();
+
+
+        }
+        catch (IOException e) {
+            System.out.println("Aaaaaaaaaaaaaaaaaaaaaaaa");
+            e.printStackTrace();
+        }
         //on press settings button
         settings_button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -45,5 +66,19 @@ public class SymptomLogger extends AppCompatActivity {
             }
         });
 
+    }
+
+    void addSymptoms() throws IOException {
+        BufferedReader bfReader = new BufferedReader(new FileReader("src\\main\\assets\\symptoms.txt"));
+        String line = bfReader.readLine();
+        while(line!=null){
+            symptoms.add(line);
+            line = bfReader.readLine();
+        }
+        bfReader.close();
+
+        for(int i=0; i<symptoms.size(); i++){
+            System.out.println(symptoms.get(i));
+        }
     }
 }
