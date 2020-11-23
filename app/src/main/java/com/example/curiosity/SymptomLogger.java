ca76fd6eb0615f0fd56b1f982e4435d8a1badf66
masafibro <arwaabdulhaq1d.aa@gmail.com>
2020-11-23 20:49:31 +0400

    checklist
    sorry its ugly

--- app/src/main/java/com/example/curiosity/SymptomLogger.java
+++ app/src/main/java/com/example/curiosity/SymptomLogger.java
@@ -3,27 +3,37 @@ package com.example.curiosity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.CheckedTextView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ScrollView;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SymptomLogger extends AppCompatActivity {
 
     ImageButton back_button;
     ImageButton settings_button;
     ListView symptomListView;
+
+    private static final String TAG = "SymptomLogger";
+
 
     List symptoms = new ArrayList();
     ArrayAdapter adapter;
@@ -37,17 +47,29 @@ public class SymptomLogger extends AppCompatActivity {
         settings_button=(ImageButton)findViewById(R.id.settings_button);
         symptomListView = (ListView) findViewById(R.id.symptomListView);
 
+        addSymptoms();
+
+//        try{
+//            addSymptoms();
+//
+//
+//        }
+//        catch (IOException e) {
+//            System.out.println("Aaaaaaaaaaaaaaaaaaaaaaaa");
+//            e.printStackTrace();
+//        }
 
 
-        try{
-            addSymptoms();
+
+        symptomListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        adapter = new ArrayAdapter(SymptomLogger.this, android.R.layout.simple_list_item_checked, symptoms);
+
+        symptomListView.setAdapter(adapter);
 
 
-        }
-        catch (IOException e) {
-            System.out.println("Aaaaaaaaaaaaaaaaaaaaaaaa");
-            e.printStackTrace();
-        }
+
+
         //on press settings button
         settings_button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -66,19 +88,86 @@ public class SymptomLogger extends AppCompatActivity {
             }
         });
 
+
+
+
+
     }
 
-    void addSymptoms() throws IOException {
-        BufferedReader bfReader = new BufferedReader(new FileReader("src\\main\\assets\\symptoms.txt"));
-        String line = bfReader.readLine();
-        while(line!=null){
-            symptoms.add(line);
-            line = bfReader.readLine();
-        }
-        bfReader.close();
 
-        for(int i=0; i<symptoms.size(); i++){
-            System.out.println(symptoms.get(i));
-        }
+
+//    void addSymptoms() throws IOException {
+//        Scanner s = new Scanner (new File("D:\\321\\symptoms"));
+//        //System.out.println(line);
+//        while(s.hasNext()){
+//            symptoms.add(s.next());
+//
+//        }
+//        s.close();
+//
+//        for(int i=0; i<symptoms.size(); i++){
+//            System.out.println(symptoms.get(i));
+//        }
+//    }
+
+    void addSymptoms(){
+        symptoms.add("Abscesses");
+        symptoms.add("Abdominal pain and discomfort");
+        symptoms.add("Abnormal lumps or masses");
+        symptoms.add("Abnormal appearance or inflammation of the eye");
+        symptoms.add("Abnormal eye movements");
+        symptoms.add("Abnormal fatigue");
+        symptoms.add("Abnormal odor");
+        symptoms.add("Abnormal thirst");
+        symptoms.add("Abnormal walking");
+        symptoms.add("Accelerated heart rate");
+        symptoms.add("Aggressive scratching");
+        symptoms.add("Aggressiveness");
+        symptoms.add("Altered behavior");
+        symptoms.add("An inflamed third eyelid that is covering a part of the infected eye");
+        symptoms.add("Anorexia");
+        symptoms.add("Blood in vomit");
+        symptoms.add("Bloody diarrhea");
+        symptoms.add("Bloody urine");
+        symptoms.add("Bow-legged appearance");
+        symptoms.add("Change in bowel or bladder habits");
+        symptoms.add("Clear, green or yellow discharge coming from the eyes");
+        symptoms.add("Cloudy eyes");
+        symptoms.add("Congestion");
+        symptoms.add("Constant scratching");
+        symptoms.add("Cough");
+        symptoms.add("Crying");
+        symptoms.add("Crying when urinating");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
+        symptoms.add("test");
     }
 }
